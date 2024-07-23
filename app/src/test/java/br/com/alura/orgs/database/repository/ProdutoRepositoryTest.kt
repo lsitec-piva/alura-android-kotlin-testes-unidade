5737a1244f1bbe9b5434a87007cb4840795c95c8
lsitec-piva <lucas.piva@lsitec.org.br>
2024-07-23 11:03:07 -0300

    aula 4

---
+++ app/src/test/java/br/com/alura/orgs/database/repository/ProdutoRepositoryTest.kt
@@ -0,0 +1,34 @@
+package br.com.alura.orgs.database.repository
+
+import br.com.alura.orgs.database.dao.ProdutoDao
+import br.com.alura.orgs.model.Produto
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+import java.math.BigDecimal
+
+class ProdutoRepositoryTest {
+
+    @Test
+    fun `deve chamar o dao quando salva um produto`() = runTest {
+        val dao = mockk<ProdutoDao>()
+        val produtoRepository = ProdutoRepository(dao)
+        val produto = Produto(
+            nome = "Banana",
+            descricao = "Banana prata",
+            valor = BigDecimal("6.99")
+        )
+
+        coEvery {
+            dao.salva(produto)
+        }.returns(Unit)
+
+        produtoRepository.salva(produto)
+
+        coVerify {
+            dao.salva(produto)
+        }
+    }
+}
